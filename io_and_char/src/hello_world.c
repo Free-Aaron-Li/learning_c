5f16d29fc058574643486b37e0ac4a87cd55203a
aaron <communicate_aaron@outlook.com>
2025-04-14 20:44:39 +0800

    测试 pre-commit 钩子

--- io_and_char/src/hello_world.c
+++ io_and_char/src/hello_world.c
@@ -5,6 +5,6 @@
 //   <communicate_aaron@outlook.com>.
 #include "../include/io_and_char.h" // 预处理指令
 
-void hello_world(void) {
+void hello_world(void) { /* void 表示无参数，这与C++中不同。C++中不加表示无参，但是在C中表示不确定 */
     printf("Hello World.\n");
 }
