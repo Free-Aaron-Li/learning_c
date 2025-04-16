fea17033692b0b89970b9d17ce481d03d6c1c875
aaron <communicate_aaron@outlook.com>
2025-04-16 15:24:20 +0800

    refactor(build): 替换xmake构建系统为CMake
    - 将原有的xmake.lua配置替换为CMakeLists.txt。
    - 支持新增CMake相关配置以适配现有项目结构。
    - 重新定义了io_and_char、expression和learning_c的目标及依赖关系。
    - 提升了构建系统的兼容性和可维护性。

--- xmake.lua
+++ xmake.lua
@@ -6,23 +6,23 @@ set_targetdir("bin")
 
 -- 子项目配置
 target("io_and_char")
-    -- 设置生成目标为可执行文件
-    set_kind("binary")
-    add_files("io_and_char/src/*.c")
-    -- 模块专用头文件目录
-    add_includedirs("io_and_char/include")
+-- 设置生成目标为可执行文件
+set_kind("binary")
+add_files("io_and_char/src/*.c")
+-- 模块专用头文件目录
+add_includedirs("io_and_char/include")
 
 target("expression")
-     -- 设置生成目标为可执行文件
-     set_kind("binary")
-     add_files("expression/src/*.c")
-     -- 模块专用头文件目录
-     add_includedirs("expression/include")
+-- 设置生成目标为可执行文件
+set_kind("binary")
+add_files("expression/src/*.c")
+-- 模块专用头文件目录
+add_includedirs("expression/include")
 
 -- 主程序配置
 target("learning_c")
-    -- 设置生成目标为可执行文件
-    set_kind("binary")
-    add_files("src/main.c")
-    add_deps("io_and_char")
-    add_deps("expression")
+-- 设置生成目标为可执行文件
+set_kind("binary")
+add_files("src/main.c")
+add_deps("io_and_char")
+add_deps("expression")
